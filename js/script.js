619cc8248b98251dd5aef408611a5da945caadd9
Joao Rodrigo <jralbertini27@gmail.com>
2023-06-19 18:21:07 -0300

    slide

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 import menuMobile from './menu-mobile.js';
-import fotos from './fotos.js';
+import scrollSuave from './scrollSuave.js';
+import { SlideNav } from './slide.js';
 
 menuMobile();
-fotos();
+
+const a = new scrollSuave('a[href^="#"')
+a.init()
+
+
+const slide = new SlideNav('.slide', '.slide-wrapper');
+slide.init();
+slide.addArrow('.prev', '.next');
+
+
+
---
+++ js/scrollSuave.js
@@ -0,0 +1,32 @@
+export default class scrollSuave{
+   constructor(link, options){
+      this.links = document.querySelectorAll(link)
+      if (options === undefined){
+         this.options = {
+            behavior: 'smooth',
+            block: 'start'
+         }
+      } else{
+         this.options = options
+      }
+   }
+
+   getHref(link){
+      link.preventDefault()
+      const hrefElement = link.target.getAttribute('href')
+      const sectionElement = document.querySelector(hrefElement)
+      sectionElement.scrollIntoView(this.options)
+   }
+
+   linkElement(){
+      this.links.forEach((link) => {
+         link.addEventListener('click', (event) => {
+            this.getHref(event)
+         })
+      })
+   }
+
+   init(){
+      this.linkElement()
+   }
+}
